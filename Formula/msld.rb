622be4b7debc6209ca1730bf29f3c632d3f01f20
felixjaschul <felix@jaschul.de>
2026-07-19 18:50:33 +0200

    v1.5.5: deep dive cleanup

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -1,8 +1,8 @@
 class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
-  url "https://github.com/xt9y/msl/archive/refs/tags/v1.5.4.tar.gz"
-  sha256 "910ac1e671014a9266f1634c78594d911bd4d232d846f46ae00c69d152707770"
+  url "https://github.com/xt9y/msl/archive/refs/tags/v1.5.5.tar.gz"
+  sha256 "3d80ecf2690e5b1234396308bb04f131e5bab5f390e1867b2bcb0b0e4b2043fc"
   license "MIT"
 
   depends_on "zig"
