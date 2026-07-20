dbb7ff12d2474082632150dbf6411fe02dc9d7dd
felixjaschul <felix@jaschul.de>
2026-07-20 14:51:56 +0200

    v1.5.10: add version update check on every command

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -1,8 +1,8 @@
 class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
-  url "https://github.com/xt9y/msl/archive/refs/tags/v1.5.9.tar.gz"
-  sha256 "d39dc135798c66b5a62ebd033958941b2fcd8edb5db5026c63160f83725face6"
+  url "https://github.com/xt9y/msl/archive/refs/tags/v1.5.10.tar.gz"
+  sha256 "c5a242ee05146eef8d4c024c66c4a101309cebf3dfae1ee74d1a7f5a2f7994cc"
   license "MIT"
 
   depends_on "zig"
