a33fe4beab3b637baa3fb304b45959ac412e8451
felixjaschul <felix@jaschul.de>
2026-07-21 14:13:25 +0200

    msl v1.6.1: round 5 pipeline fixes (use commit SHA URL to bypass CDN cache)

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -1,8 +1,9 @@
 class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
-  url "https://github.com/xt9y/msl/archive/refs/tags/v1.6.0.tar.gz"
-  sha256 "505a8b848c34ac7c94a89df1433fe75ba0f81d3b6165e6a1c6cfd20831dbf179"
+  url "https://github.com/xt9y/msl/archive/4e02a3e472495b947fcdb6068c83f8c92ee565e0.tar.gz"
+  version "1.6.1"
+  sha256 "3c65a7b33765bcc6ac69f5cf8f1498a6faf518ecde39960db3ed6123c728a95c"
   license "MIT"
 
   depends_on "zig"
