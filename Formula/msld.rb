889065b24ec6f51d0345f055ba8ce94148d742a9
Felix <felix@xt9y.de>
2026-07-21 17:20:17 +0200

    msld: update to 1.7.1

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -1,8 +1,8 @@
 class Msld < Formula
-  desc "Guest daemon for msl (MacOS Subsystem for Linux)"
+  desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
-  url "https://github.com/xt9y/msl/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "d5b976714cd2683a6181c71dbadfd18d173a6f758d04f3ed513065d68fc98352"
+  url "https://github.com/xt9y/msl/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "1990991778e3ace95ecd35eeb02abd1a995daaf74b62ee459efb1cd18e8eb86a"
   license "MIT"
 
   depends_on "zig"
