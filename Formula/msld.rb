4e8153df0db135b2897ece663e29afec02938ca1
felixjaschul <felix@jaschul.de>
2026-07-23 00:04:28 +0200

    v0.7.22: pre-configured rootfs download from GitHub releases

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -1,8 +1,8 @@
 class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
-  url "https://github.com/xt9y/msl/archive/refs/tags/v0.7.35.tar.gz"
-  sha256 "15e0cff7d369202202c260317d72330ac7e08a2c15ab3307bf8b48754dd678fd"
+  url "https://github.com/xt9y/msl/archive/refs/tags/v0.7.22.tar.gz"
+  sha256 "d96aeda742bab84ccf80f169100ed4f3095c41330a243386a8e0c2ec52106ef0"
   license "MIT"
 
   depends_on "zig"
