9a77617f8b4828c11c9e20d6851fceb489eaaf1b
felixjaschul <felix@jaschul.de>
2026-07-22 16:34:52 +0200

    v0.7.22: fix fgets line input race, user auth prompts

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -2,7 +2,7 @@ class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
   url "https://github.com/xt9y/msl/archive/refs/tags/v0.7.22.tar.gz"
-  sha256 "b018e62a288e1f2d8f4fdf6102ce94bc658f7d3ea86a59205239baa56f0fd0dd"
+  sha256 "b0c8aea3115a9f9bddbe7d200a25e8df3b0a6e8c86d0ba0e0e3fe5e39e11483f"
   license "MIT"
 
   depends_on "zig"
