575a39ee13df809f3668de88ae6605d1f9e31d1b
felixjaschul <felix@jaschul.de>
2026-07-23 00:09:05 +0200

    v0.7.22: update SHA256 (forced push changed tarball)

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -2,7 +2,7 @@ class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
   url "https://github.com/xt9y/msl/archive/refs/tags/v0.7.22.tar.gz"
-  sha256 "2aa015bd831169dd4ce836b2847dcdff48d2e26bd0f041c3b3f8127a705960bd"
+  sha256 "d482e35c31ae71038c1c7f9a4bc85fd1f99238c613d37f8361fbab81cbfdccb5"
   license "MIT"
 
   depends_on "zig"
